ec1c4f3207cebb41a27b56fe35f1e77e458bd2e1
mekingme <sp8083@naver.com>
2019-10-22 17:16:52 +0900

    test

--- main.go
+++ main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/waker_server/cmd"
+	"github.com/go-redis/redis/v7"
+	//"github.com/gin-contrib/sessions"
+	//"github.com/gin-contrib/sessions/redis"
 )
 
 func main() {
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:               "localhost:6379",
+		Password:			"",
+		DB:                 0,
+	})
+
+	_, err := redisClient.Ping().Result()
+	if err != nil{
+		panic(err)
+	}
 
 	router := gin.Default()
 
